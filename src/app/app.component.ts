be975c3c464929dbd09901093d4a0d77ef6ba935
Shelly Xiong <shelly.l.xiong@gmail.com>
2020-07-25 11:06:16 -0500

    Your Message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>
+    Welcome to {{ title }}!
+    </h1>
+
+  <app-input-button-unit></app-input-button-unit>
+  `,
 })
 export class AppComponent {
   title = 'todo-list';
